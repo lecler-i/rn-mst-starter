99b867be467178dcb44b20ede5e31ab39ad23996
Thomas LECLERCQ <contact@thomas.sh>
2017-10-29 11:09:10 +0530

    Add Stolen Button component

---
+++ src/components/ui/Button.js
@@ -0,0 +1,87 @@
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ViewPropTypes,
+  ActivityIndicator
+} from 'react-native';
+
+const systemButtonOpacity = 0.2;
+
+export default class Button extends Component {
+  static propTypes = {
+    ...TouchableOpacity.propTypes,
+    allowFontScaling: Text.propTypes.allowFontScaling,
+    containerStyle: ViewPropTypes.style,
+    loading: PropTypes.bool,
+    disabled: PropTypes.bool,
+    style: Text.propTypes.style,
+    styleDisabled: Text.propTypes.style
+  };
+
+  render() {
+    let touchableProps = {
+      activeOpacity: this._computeActiveOpacity()
+    };
+    if (this.props.loading) {this.props.disabled = true;}
+    if (!this.props.disabled) {
+      touchableProps.onPress = this.props.onPress;
+      touchableProps.onPressIn = this.props.onPressIn;
+      touchableProps.onPressOut = this.props.onPressOut;
+      touchableProps.onLongPress = this.props.onLongPress;
+      touchableProps.delayPressIn = this.props.delayPressIn;
+      touchableProps.delayPressOut = this.props.delayPressOut;
+      touchableProps.delayLongPress = this.props.delayLongPress;
+    }
+
+    return (
+      <TouchableOpacity
+        {...touchableProps}
+        testID={this.props.testID}
+        style={this.props.containerStyle}
+        accessibilityTraits='button'
+        accessibilityComponentType='button'>
+        {this.props.loading ? <ActivityIndicator /> : this.props.children}
+      </TouchableOpacity>
+    );
+  }
+
+  _renderGroupedChildren() {
+    let {disabled} = this.props;
+    let style = [
+      styles.text,
+      disabled ? styles.disabledText : null,
+      this.props.style,
+      disabled ? this.props.styleDisabled : null
+    ];
+  }
+
+  _computeActiveOpacity() {
+    if (this.props.disabled) {
+      return 1;
+    }
+    return this.props.activeOpacity != null
+      ? this.props.activeOpacity
+      : systemButtonOpacity;
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#007aff',
+    fontSize: 17,
+    fontWeight: '500',
+    textAlign: 'center'
+  },
+  disabledText: {
+    color: '#dcdcdc'
+  },
+  group: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
+});
